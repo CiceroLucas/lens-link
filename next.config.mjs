eee791ba498fc76494afe565cf85ffae2d5ab222
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-08-01 18:30:41 -0300

    update config and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zavxbzloccalzprhjzkt.supabase.co",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
