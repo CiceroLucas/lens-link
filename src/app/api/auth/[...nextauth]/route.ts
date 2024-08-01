802b003f4f3551e3586444e1e48393a898d10271
CiceroLucas <lukasferreiradesousa890@gmail.com>
2024-08-01 18:31:32 -0300

    feat: add auth config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch(
+          `https://lens-link-api.onrender.com/api/v1/auth/login`,
+          {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        const user = await res.json();
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
